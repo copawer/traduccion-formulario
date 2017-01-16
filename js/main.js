be96c5968bb45091a85dcf17e7cdb3e87ed3cafd
Sara Gabriela Díaz Oporto <sara.diaz.oporto@gmail.com>
2017-01-16 12:51:36 -0500

    ejercicio1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById("form-signin-heading").innerHtml="por favor inicia sesion";
+document.getElementById("inputEmail").placeholder="por favor ingrese su correo";
+document.getElementById("inputPassword").placeholder='contraseña';
+document.getElementById("Span").innerHTML="recordar";
+document.getElementById("button").innerHTML="iniciar sesion";
